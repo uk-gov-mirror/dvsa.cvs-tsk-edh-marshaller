7a77f5d98621cb9e3953c9f3caaff2c7817edac5
Catalin-Andrei Stancu <castancu@deloittece.com>
2020-08-13 14:00:57 +0300

    retry only 5XX and 429

--- src/functions/edhMarshaller.ts
+++ src/functions/edhMarshaller.ts
@@ -13,7 +13,7 @@ import {StreamRecord} from "../models";
  * @param context - λ Context
  * @param callback - callback function
  */
-const edhMarshaller: Handler = async (event: GetRecordsOutput, context?: Context, callback?: Callback): Promise<void | Array<PromiseResult<SendMessageResult, AWSError>>> => {
+const edhMarshaller: Handler = async (event: GetRecordsOutput, context?: Context, callback?: Callback): Promise<void | Array<PromiseResult<SendMessageResult, AWSError>>> => {
     if (!event) {
         console.error("ERROR: event is not defined.");
         return;
@@ -32,7 +32,7 @@ const edhMarshaller: Handler = async (event: GetRecordsOutput, context?: Context
 
     records.forEach((record: StreamRecord) => {
         debugOnlyLog("Record: ", record);
-        debugOnlyLog("New image: ", record.dynamodb?.NewImage)
+        debugOnlyLog("New image: ", record.dynamodb?.NewImage);
         const targetQueue = getTargetQueueFromSourceARN(record.eventSourceARN);
         debugOnlyLog("Target Queue", targetQueue);
         const eventType = record.eventName; //INSERT, MODIFY or REMOVE
@@ -45,14 +45,19 @@ const edhMarshaller: Handler = async (event: GetRecordsOutput, context?: Context
     });
 
     return Promise.all(sendMessagePromises)
-    .catch((error: AWSError) => {
-        console.error(error);
-        console.log("records");
-        console.log(records);
-        if(error.code !== "InvalidParameterValue"){
-            throw error;
-        }
-    });
+        .catch((error: AWSError) => {
+            console.error(error);
+            console.log("records");
+            console.log(records);
+            // do not retry 4XX error codes, except 429. Retry only 500 and 429.
+            if ((error.statusCode >= 400 && error.statusCode < 500) && error.statusCode !== 429) {
+                // return something so the Lambda won't retry
+                return;
+            } else {
+                // throw the error so the Lambda will retry
+                throw error;
+            }
+        });
 };
 
 export {edhMarshaller};
--- tests/unit/edhMarshallerFunction.unitTest.ts
+++ tests/unit/edhMarshallerFunction.unitTest.ts
@@ -1,5 +1,4 @@
 import {edhMarshaller} from "../../src/functions/edhMarshaller";
-import mockContext from "aws-lambda-mock-context";
 import {SQService} from "../../src/services/SQService";
 import {Configuration} from "../../src/utils/Configuration";
 import {Context} from "aws-lambda";
@@ -50,15 +49,40 @@ describe("edhMarshaller Function", () => {
     });
 
     describe("when SQService throws an error", () => {
-      it("should throw the error up", async () => {
-        jest.spyOn(SQService.prototype, "sendMessage").mockRejectedValue("It broke");
+      context("and the error is from 4XX error family, except 429", () => {
+        it("should NOT throw the error up and return undefined so the Lambda won't retry", async () => {
+          jest.spyOn(SQService.prototype, "sendMessage").mockRejectedValue({statusCode: 413, message: "It broke"});
 
-        try {
-          await edhMarshaller(event, ctx, () => { return; });
-        } catch (e) {
-          expect(e).toEqual("It broke");
-        }
+            const result = await edhMarshaller(event, ctx, () => { return; });
+            expect(result).toEqual(undefined);
+        });
       });
-    })
+
+      context("and the error is 429", () => {
+        it("should throw the error up", async () => {
+          jest.spyOn(SQService.prototype, "sendMessage").mockRejectedValue({statusCode: 429, message: "It broke"});
+
+          try{
+            expect(await edhMarshaller(event, ctx, () => { return; })).toThrowError();
+          } catch(e) {
+            expect(e.statusCode).toEqual(429);
+            expect(e.message).toEqual("It broke");
+          }
+        });
+      });
+
+      context("and the error is from 5XX error family", () => {
+        it("should throw the error up", async () => {
+          jest.spyOn(SQService.prototype, "sendMessage").mockRejectedValue({statusCode: 500, message: "It broke"});
+
+          try{
+            expect(await edhMarshaller(event, ctx, () => { return; })).toThrowError();
+          } catch(e) {
+            expect(e.statusCode).toEqual(500);
+            expect(e.message).toEqual("It broke");
+          }
+        });
+      });
+    });
   });
 });
