e054bb6e0169cfc2582fd784e7b72ecc8af2f836
Prem Ghinde <github@nearlydone.co.uk>
2022-06-06 10:57:11 +0100

    feat(cb2-4032): Unit test count appearing in Sonarqube (#25)

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   setupFiles: ['jest-plugin-context/setup'],
   moduleFileExtensions: ['js', 'ts'],
+  coverageReporters: ["clover", "json", "lcov", "text", "text-summary"],
+  collectCoverage: true,
   testResultsProcessor: 'jest-sonar-reporter',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
